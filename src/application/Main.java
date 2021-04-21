2f16e5cc3bd9cfcbbb288a947edb9d4af197cd99
unknown <vitor_fabre@hotmail.com>
2021-04-21 00:16:33 -0300

    Ajuste nomenclatura título tela MainView

--- src/application/Main.java
+++ src/application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 			Parent parent = loader.load();
 			Scene mainScene = new Scene(parent);
 			primaryStage.setScene(mainScene);
-			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.setTitle("CRUD Contas a Pagar - Nexxera");
 			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
